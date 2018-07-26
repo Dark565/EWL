8954e8576691f5f5c3a0bbfdcc1052a14d827b1e
Dark565 <georgekociolek@gmail.com>
2018-07-26 04:25:34 +0200

    Loading fixes

--- source/QL/dynamic_lib.hpp
+++ source/QL/dynamic_lib.hpp
@@ -24,7 +24,7 @@ namespace ql {
         {
             auto* lib = getLibraryHash();
             if(lib->find(i) == lib->end()) {
-                return lib->insert(std::pair<int,ql::Library>(i,{}))->second;
+                return lib->insert(std::pair<int,ql::Library>(i,ql::Library()))->second;
             }
             return lib->at(i);
         }
